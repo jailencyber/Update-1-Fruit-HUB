8a759e80cf556a2e544c445c8c74883233e2cc2b
jailencyber <leotempapi@gmail.com>
2025-02-10 01:51:53 -0600

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jailencyber/Update-1-Fruit-HUB/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jailencyber/Update-1-Fruit-HUB/tree/main.lua"))()
